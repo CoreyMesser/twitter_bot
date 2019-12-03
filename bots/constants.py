74f9ea43d64ce2d5ca0e58895267fbb5170d4ebe
pibblefiasco <tiredorangecat@gmail.com>
2019-12-03 15:33:21 -0600

    Phase 1 : Building Retrieve/Follow/List

---
+++ bots/constants.py
@@ -0,0 +1,4 @@
+import os
+
+class Constants(object):
+    LIST_ID = os.environ('LIST_ID')
--- bots/followfollowers.py
+++ bots/followfollowers.py
@@ -3,8 +3,12 @@ import logging
 from bots.config import create_api
 import time
 
+from bots.constants import Constants
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
+
+con = Constants()
 
 def follow_followers(api):
     logger.info("Retrieving and following followers")
@@ -13,6 +17,22 @@ def follow_followers(api):
             logger.info(f"Following {follower.name}")
             follower.follow()
 
+def check_friend(api,user):
+    status = api.show_friendship(user)
+    return status['relationship']['source']['following']
+
+def make_friend(api, user):
+    api.create_friendship(screen_name=user)
+
+def update_friend(api, user):
+    api.create_mute(screen_name=user)
+    api.add_list_member(list_id=con.LIST_ID, screen_name=user)
+
+def get_user_list():
+    # gets list from db
+    sql = """
+    SELECT artists FROM public.artists WHERE artist_twitter not like 'None'
+    """
 def main():
     api = create_api()
     while True:
